a3d59abb61f68911659f1d01d84a27fc3287e512
Muhammad Ishaque <afnanhafeeztareen@gmail.com>
2025-07-19 15:28:45 +0500

    Initial commit of personal portfolio

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// main.js
+// Currently no interactivity is required.
+// This file is linked in index.html and reserved for future enhancements.
+
+console.log("Portfolio website loaded successfully.");
